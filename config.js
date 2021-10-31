a3f4de90267fa0d82383072dc1207c7753e1ee97
Luke Walter <lwalter5667@gmail.com>
2021-10-31 15:47:13 -0400

    Connected bot to Twitter account

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  CONSUMER_KEY:         'Zjypct8wfMHeYrsnjqODEq4Oh',
-  CONSUMER_SECRET:      'JKXBGRXVVouVHyb6NriDnn4kZNVtB56tXOKR2GAhKfdGE6VEmG',
-  BEARER_TOKEN:         'AAAAAAAAAAAAAAAAAAAAAOFEVQEAAAAA0XY4cAwM8p3rYTi%2F%2BSwjm%2Fc1Jjk%3DMXDhK4VjzvAEi4OyEAtCiLxSDRF8OUiefDBSVW6NYX8XlOsMoA',
+  consumer_key:         'Zjypct8wfMHeYrsnjqODEq4Oh',
+  consumer_secret:      'JKXBGRXVVouVHyb6NriDnn4kZNVtB56tXOKR2GAhKfdGE6VEmG',
+  access_token:         '1454167519682465793-lKMuZovMMt1U32IDDhVQisnAp92b49',
+  access_token_secret:        'E1NGXn9elSGoUJlxjPDLWXZLcbgFwwZZWtkfphdLnZtAJ',
+  bearer_token:         'AAAAAAAAAAAAAAAAAAAAAOFEVQEAAAAA0XY4cAwM8p3rYTi%2F%2BSwjm%2Fc1Jjk%3DMXDhK4VjzvAEi4OyEAtCiLxSDRF8OUiefDBSVW6NYX8XlOsMoA',
 }

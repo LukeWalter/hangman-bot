7c2d6aac4a14aef74787f3723e5ae92f6caff32c
Luke Walter <lwalter5667@gmail.com>
2021-11-01 17:54:10 -0400

    Added @mention functionality

--- bot.js
+++ bot.js
@@ -170,38 +170,36 @@ function getInput() {
 
 //play(new Hangman("bigchungus"));
 
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
+var stream = T.stream('statuses/filter', { track: ['@hang_man_bot'] });
+stream.on('tweet', tweetEvent);
 
-// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
-function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
-	  // log out any errors and responses
-	  console.log(error, data);
-	  // If our search request to the server had no errors...
-	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	});
-}
+function tweetEvent(tweet) {
 
-// Try to retweet something as soon as we run the program...
-retweetLatest();
-// ...and then every hour after that. Time here is in milliseconds, so
-// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+    // Who sent the tweet?
+    var name = tweet.user.screen_name;
+    // What is the text?
+    // var txt = tweet.text;
+    // the status update or tweet ID in which we will reply
+    var nameID  = tweet.id_str;
+
+     // Get rid of the @ mention
+    // var txt = txt.replace(/@myTwitterHandle/g, "");
+
+    // Start a reply back to the sender
+    var reply = "You mentioned me! @" + name + ' ' + 'You are super cool!';
+    var params             = {
+                              status: reply,
+                              in_reply_to_status_id: nameID
+                             };
+
+    T.post('statuses/update', params, function(err, data, response) {
+      if (err !== undefined) {
+        console.log(err);
+      } else {
+        console.log('Tweeted: ' + params.status);
+      }
+    })
+};
+
+
+console.log("bot is running");
